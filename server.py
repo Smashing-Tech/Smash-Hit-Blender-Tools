28f87605bd966b01c6d5e4a9251a0b5f78e12969
knot126 <73200930+knot126@users.noreply.github.com>
2023-02-15 03:09:33 +0000

    Reworking the quick test server, new Quick Test Client release

--- server.py
+++ server.py
@@ -1,5 +1,10 @@
 """
 HTTP server for test mod
+
+Notes:
+
+ - Smash Hit does not actually implement the Host header correctly and
+   excludes the port. We have to fix that.
 """
 
 from http.server import BaseHTTPRequestHandler, HTTPServer
@@ -8,10 +13,13 @@ import socket
 import tempfile
 import xml.etree.ElementTree as et
 import common
+from urllib.parse import parse_qs
+import pathlib
 import os
+import os.path
 
 CONTENT_LEVEL = """<level>
-	<room type="http://{}:8000/room?youare={}&amp;ignore=" distance="1000" start="true" end="true" />
+	<room type="http://{}:8000/room?ignore=" distance="1000" start="true" end="true" />
 </level>"""
 
 TEMPDIR = tempfile.gettempdir() + "/shbt-testserver/"
@@ -22,16 +30,11 @@ def parsePath(url):
 	"""
 	
 	url = url.split("?")
-	params = {}
+	params = parse_qs(url[1]) if len(url) > 1 else {}
 	
-	if (len(url) >= 2):
-		url[1] = url[1].split("&")
-		
-		for param in url[1]:
-			p = param.split("=")
-			key = p[0]
-			value = p[1]
-			params[key] = value
+	# Only use the first one
+	for p in params:
+		params[p] = params[p][0]
 	
 	url = url[0]
 	
@@ -42,29 +45,7 @@ def loadFileBytes(path):
 	Load a file's bytes.
 	"""
 	
-	f = open(path, "rb")
-	content = f.read()
-	f.close()
-	
-	return content
-
-def log(*args):
-	"""
-	Write content to a log file
-	"""
-	
-	content = ""
-	
-	for a in args:
-		content += format(a) + (" " if a != args[-1] else "")
-	
-	if (common.PRINT_LOGGING):
-		print(content)
-	
-	if (common.FILE_LOGGING):
-		f = open(TEMPDIR + "server.log", "a")
-		f.write(content + "\n")
-		f.close()
+	return pathlib.Path(path).read_bytes()
 
 def getSegmentOptions(path):
 	"""
@@ -94,16 +75,17 @@ def generateRoomText(hostname, options):
 	music = ("\"" + music + "\"") if music else "tostring(math.random(0, 28))"
 	particles = (f"\n\tmgParticles(\"{particles}\")") if particles else ""
 	reverb = (f"\n\tmgReverb({reverb})") if reverb else ""
+	length = 90
 	
 	room = f"""function init()
 	mgMusic({music})
 	mgFogColor({options["fog"]}){reverb}{particles}
 	
-	confSegment("http://{hostname}:8000/segment?youare={hostname}&filetype=", 1)
+	confSegment("http://{hostname}:8000/segment?filetype=", 1)
 	
 	l = 0
 	
-	local targetLen = 90
+	local targetLen = {length}
 	while l < targetLen do
 		s = nextSegment()
 		l = l + mgSegment(s, -l)
@@ -117,6 +99,14 @@ end"""
 	
 	return bytes(room, "utf-8")
 
+def doError(self):
+	data = bytes("404 File Not Found", "utf-8")
+	self.send_response(404)
+	self.send_header("Content-Length", str(len(data)))
+	self.send_header("Content-Type", "text/plain")
+	self.end_headers()
+	self.wfile.write(data)
+
 class AdServer(BaseHTTPRequestHandler):
 	"""
 	The request handler for the test server
@@ -126,22 +116,29 @@ class AdServer(BaseHTTPRequestHandler):
 		pass
 	
 	def do_GET(self):
+		# Log the request
+		print(self.client_address[0] + ":" + str(self.client_address[1]), self.command, self.path)
+		
 		# Set data
 		data = b""
 		contenttype = "text/xml"
 		
-		# Hacky way of parsing parameters
+		# Parsing parameters
 		path, params = parsePath(self.path)
+		
+		# Get the host's name (that is us!)
+		# Taking only the IP makes nonbugged clients (e.g. not SH) work.
+		host = self.headers["Host"].split(":")[0]
 		
 		# Handle what data to return
 		try:
 			### LEVEL ###
 			if (path.endswith("level")):
-				data = bytes(CONTENT_LEVEL.format(params["youare"], params["youare"]), "utf-8")
+				data = bytes(CONTENT_LEVEL.format(host), "utf-8")
 			
 			### ROOM ###
 			elif (path.endswith("room")):
-				data = generateRoomText(params["youare"], getSegmentOptions(TEMPDIR + "segment.xml"))
+				data = generateRoomText(host, getSegmentOptions(TEMPDIR + "segment.xml"))
 				contenttype = "text/plain"
 			
 			### SEGMENT ###
@@ -152,14 +149,13 @@ class AdServer(BaseHTTPRequestHandler):
 			elif (path.endswith("segment") and (params["filetype"] == ".mesh")):
 				data = loadFileBytes(TEMPDIR + "segment.mesh")
 				contenttype = "application/octet-stream"
+			
+			### MENU UI ###
+			elif (path.endswith("menu")):
+				data = bytes(f'''<ui texture="menu/start.png" selected="menu/button_select.png"><rect coords="0 0 294 384" cmd="level.start level:http://{host}:8000/level?ignore="/></ui>''', "utf-8")
 		except:
 			# Error on other files
-			data = bytes("<html><head><title>404 File Not Found</title></head><body><h1>404 File Not Found</h1><p><i>Smash Hit Blender Tools - Test Server Module</i></p></body></html>", "utf-8")
-			self.send_response(404)
-			self.send_header("Content-Length", str(len(data)))
-			self.send_header("Content-Type", "text/html")
-			self.end_headers()
-			self.wfile.write(data)
+			doError(self)
 			return
 		
 		# Send response
@@ -168,31 +164,39 @@ class AdServer(BaseHTTPRequestHandler):
 		self.send_header("Content-Type", contenttype)
 		self.end_headers()
 		self.wfile.write(data)
-		
-		# Log the request
-		log(self.client_address[0] + ":" + str(self.client_address[1]), self.command, self.path, "-> 200 OK")
-		log(" ------------------------------------------------------------- ")
-		log(format(data))
-		log("\n\n\n")
 
-def debug_generate_room():
-	f = open(TEMPDIR + "/room.lua", "wb")
-	f.write(generateRoomText("owo", getSegmentOptions(TEMPDIR + "segment.xml")))
-	f.close()
+def makeTestFiles():
+	"""
+	Create test files
+	"""
+	
+	print("SegServ: Creating test files...")
+	
+	# Make the folder itself
+	os.makedirs(TEMPDIR, exist_ok = True)
+	
+	# Make test segment
+	pathlib.Path(TEMPDIR + "segment.xml").write_text('<segment size="12 10 16"><box pos="0 -1 -1" size="0.5 0.5 0.5" visible="1" color="0.3 0.6 0.9" tile="63"/></segment>')
+	
+	# Cook mesh for it
+	os.system(f"python3 ./bake_mesh.py {TEMPDIR + 'segment.xml'} {TEMPDIR + 'segment.mesh'}")
 
-def runServer():
+def runServer(no_blender = False):
 	"""
 	Run the server
 	"""
 	
 	server = HTTPServer(("0.0.0.0", 8000), AdServer)
 	
-	log("Smash Hit Tools: Server running!")
+	if (no_blender):
+		makeTestFiles()
+	
+	print("** SegServ v1.0 - Smash Hit Quick Test Server **")
 	
 	try:
 		server.serve_forever()
 	except Exception as e:
-		log("Smash Hit Tools: Test server is down:\n\n", e)
+		print("SegServ has crashed!!\n", e)
 	
 	server.server_close()
 
@@ -206,4 +210,4 @@ def runServerProcess():
 	return p
 
 if (__name__ == "__main__"):
-	runServer()
+	runServer(no_blender = True)
